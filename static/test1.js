4d09d60f047bb7cc549bd83fdc4f1d2d27cf2d80
Hoang Pham Truc Phuong (CME.MSA) <hptphuong@gmail.com>
2017-10-17 16:45:28 +0700

    update google js

--- static/test1.js
+++ static/test1.js
@@ -1,6 +1,7 @@
 (function() {
   var img = new Image,
-      url = encodeURIComponent(document.location.href),
+      // url = encodeURIComponent(document.location.href),
+      url = encodeURIComponent("https://phuonganalytic.herokuapp.com"),
       title = encodeURIComponent(document.title),
       ref = encodeURIComponent(document.referrer);
       img.src = 'phuonganalytic.herokuapp.com/a.gif?url=' + url + '&t=' + title + '&ref=' + ref;
