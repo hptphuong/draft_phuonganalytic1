e7eb652720644b8b79d0d797b84da572e97d31e4
Hoang Pham Truc Phuong (CME.MSA) <hptphuong@gmail.com>
2017-12-11 16:58:46 +0700

    fix a litle

--- api/views.py
+++ api/views.py
@@ -121,7 +121,7 @@ def userDailyReportList(request):
 	if request.method == 'POST':
 		data = JSONParser().parse(request)
 		if(len(data['x1_start'])==0 or len(data['x1_end']) or len(data['x2_start']) or len(data['x2_end'])):
-			return JsonResponse('not support', status=400)
+			return JsonResponse({'status':'false','message':'null querry'}, status=400)
 		utczone = tz.gettz('UTC')
 		x1_start=int(datetime.strptime(data['x1_start'][0],'%Y-%m-%d').replace(tzinfo=utczone).timestamp())
 		x1_end = int(datetime.strptime(data['x1_end'][0], '%Y-%m-%d').replace(tzinfo=utczone).timestamp())
